4d9ac3bb7905397853fb6e075ed4fd2b7e4b3d1a
Madusha95 <nayanapriyasuraweera95@gmail.com>
2025-06-02 23:10:32 +0530

    Add files via upload

---
+++ Client1/Client1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Client1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(266, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 49);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(419, 92);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 49);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(266, 156);
+            button3.Name = "button3";
+            button3.Size = new Size(105, 49);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveBorder;
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(419, 156);
+            button4.Name = "button4";
+            button4.Size = new Size(105, 49);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveBorder;
+            button5.ForeColor = SystemColors.ActiveCaptionText;
+            button5.Location = new Point(266, 229);
+            button5.Name = "button5";
+            button5.Size = new Size(105, 49);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveBorder;
+            button6.ForeColor = SystemColors.ActiveCaptionText;
+            button6.Location = new Point(419, 229);
+            button6.Name = "button6";
+            button6.Size = new Size(105, 49);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ActiveBorder;
+            button7.ForeColor = SystemColors.ActiveCaptionText;
+            button7.Location = new Point(266, 302);
+            button7.Name = "button7";
+            button7.Size = new Size(105, 49);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveBorder;
+            button8.ForeColor = SystemColors.ActiveCaptionText;
+            button8.Location = new Point(419, 302);
+            button8.Name = "button8";
+            button8.Size = new Size(105, 49);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveBorder;
+            button9.ForeColor = SystemColors.ActiveCaptionText;
+            button9.Location = new Point(266, 363);
+            button9.Name = "button9";
+            button9.Size = new Size(105, 49);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveBorder;
+            button10.ForeColor = SystemColors.ActiveCaptionText;
+            button10.Location = new Point(419, 363);
+            button10.Name = "button10";
+            button10.Size = new Size(105, 49);
+            button10.TabIndex = 9;
+            button10.Text = "10";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Client1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+}
---
+++ Client1/Client1/Form1.cs
@@ -0,0 +1,336 @@
+using System.IO;
+using System.Net.Sockets;
+using System;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Client1
+{
+    public partial class Form1 : Form
+    {
+        private bool isOn1 = false;
+        private bool isOn2 = false;
+        private bool isOn3 = false;
+        private bool isOn4 = false;
+        private bool isOn5 = false;
+        private bool isOn6 = false;
+        private bool isOn7 = false;
+        private bool isOn8 = false;
+        private bool isOn9 = false;
+        private bool isOn10 = false;
+
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread receiveThread;
+
+        private bool[] buttonStates = new bool[10];
+
+        private bool isBusy = false;
+        private readonly object lockObject = new object(); // Lock for busy state management  
+        private readonly object streamLock = new object(); // Separate lock for stream operations
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+            button1.Text = "OFF";
+
+            button2.BackColor = Color.Red;
+            button2.Text = "OFF";
+
+            button3.BackColor = Color.Red;
+            button3.Text = "OFF";
+
+            button4.BackColor = Color.Red;
+            button4.Text = "OFF";
+
+            button5.BackColor = Color.Red;
+            button5.Text = "OFF";
+
+            button6.BackColor = Color.Red;
+            button6.Text = "OFF";
+
+            button7.BackColor = Color.Red;
+            button7.Text = "OFF";
+
+            button8.BackColor = Color.Red;
+            button8.Text = "OFF";
+
+            button9.BackColor = Color.Red;
+            button9.Text = "OFF";
+
+            button10.BackColor = Color.Red;
+            button10.Text = "OFF";
+
+            // Connect to server
+            try
+            {
+                client = new TcpClient("127.0.0.1", 5000);  // Change to server IP if remote
+                stream = client.GetStream();
+
+                receiveThread = new Thread(ReceiveData);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not connect to server: " + ex.Message);
+            }
+        }
+
+        //private void ReceiveData()
+        //{
+        //    byte[] buffer = new byte[1024];
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        //            if (bytesRead == 0) continue;
+
+        //            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        //            if (message.Trim() == "ALL" || message.Trim() == "CLIENT1")
+        //            {
+        //                string status = GetButtonStatuses();
+        //                byte[] dataToSend = Encoding.ASCII.GetBytes("CLIENT1"+status);
+        //                stream.Write(dataToSend, 0, dataToSend.Length);
+        //            }
+        //        }
+        //        catch
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+        private void ReceiveData()
+        {
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    // Check if connection was closed by server
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim();
+
+                    // Handle each message in a separate thread
+                    Thread messageThread = new Thread(() => HandleMessage(message))
+                    {
+                        IsBackground = true,
+                        Name = $"MessageHandler-{DateTime.Now.Ticks}"
+                    };
+                    messageThread.Start();
+                }
+                catch (System.IO.IOException ioEx)
+                {
+                    break;
+                }
+                catch (System.Net.Sockets.SocketException sockEx)
+                {
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+            }
+
+            // Clean up connection
+            try
+            {
+                stream?.Close();
+                client?.Close();
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private async void HandleMessage(string message)
+        {
+            try
+            {
+                // Handle status request commands
+                if (message.Trim() == "ALL")
+                {
+                    bool shouldProcess = false;
+
+                    lock (lockObject)
+                    {
+                        // Check if already processing a request
+                        if (isBusy)
+                        {
+                            // Send busy message immediately
+                            byte[] busyMessage = Encoding.ASCII.GetBytes("CLIENT1BUSY");
+                            lock (streamLock) // Use dedicated stream lock
+                            {
+                                stream.Write(busyMessage, 0, busyMessage.Length);
+                                stream.Flush();
+                            }
+                            return;
+                        }
+
+                        // Set busy flag
+                        isBusy = true;
+                        shouldProcess = true;
+                    }
+
+                    if (shouldProcess)
+                    {
+                        try
+                        {
+                            // Step 1: Send Acknowledgement immediately
+                            byte[] ackMessage = Encoding.ASCII.GetBytes("CLIENT1ACK");
+                            lock (streamLock) // Use dedicated stream lock
+                            {
+                                stream.Write(ackMessage, 0, ackMessage.Length);
+                                stream.Flush();
+                            }
+
+                            // Step 2: Wait 3 seconds
+                            await Task.Delay(3000);
+
+                            // Step 3: Send Reply with button status
+                            string status = GetButtonStatuses();
+                            byte[] dataToSend = Encoding.ASCII.GetBytes("CLIENT1" + status);
+                            lock (streamLock) // Use dedicated stream lock
+                            {
+                                stream.Write(dataToSend, 0, dataToSend.Length);
+                                stream.Flush();
+                            }
+                        }
+                        finally
+                        {
+                            lock (lockObject)
+                            {
+                                isBusy = false;
+                            }
+                        }
+                    }
+                }
+                else if (!string.IsNullOrEmpty(message))
+                {
+                    // Handle other messages - send acknowledgement
+                    byte[] ackMessage = Encoding.ASCII.GetBytes("CLIENT1ACK");
+                    lock (streamLock) // Use dedicated stream lock
+                    {
+                        stream.Write(ackMessage, 0, ackMessage.Length);
+                        stream.Flush();
+                    }
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+        private string GetButtonStatuses()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append($"Button{i + 1}={(buttonStates[i] ? "ON" : "OFF")},");
+            }
+            return sb.ToString().TrimEnd(',');
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Toggle the boolean state isOn1: if true, becomes false; if false, becomes true
+            isOn1 = !isOn1;
+            button1.BackColor = isOn1 ? Color.Green : Color.Red;
+            button1.Text = isOn1 ? "ON" : "OFF"; // Update the button text to display the current state: "ON" or "OFF"
+            buttonStates[0] = isOn1;    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isOn2 = !isOn2;
+            button2.BackColor = isOn2 ? Color.Green : Color.Red;
+            button2.Text = isOn2 ? "ON" : "OFF"; 
+            buttonStates[1] = isOn2;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isOn3 = !isOn3;
+            button3.BackColor = isOn3 ? Color.Green : Color.Red;
+            button3.Text = isOn3 ? "ON" : "OFF";
+            buttonStates[2] = isOn3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            isOn4 = !isOn4;
+            button4.BackColor = isOn4 ? Color.Green : Color.Red;
+            button4.Text = isOn4 ? "ON" : "OFF";
+            buttonStates[3] = isOn4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            isOn5 = !isOn5;
+            button5.BackColor = isOn5 ? Color.Green : Color.Red;
+            button5.Text = isOn5 ? "ON" : "OFF";
+            buttonStates[4] = isOn5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            isOn6 = !isOn6;
+            button6.BackColor = isOn6 ? Color.Green : Color.Red;
+            button6.Text = isOn6 ? "ON" : "OFF";
+            buttonStates[5] = isOn6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            isOn7 = !isOn7;
+            button7.BackColor = isOn7 ? Color.Green : Color.Red;
+            button7.Text = isOn7 ? "ON" : "OFF";
+            buttonStates[6] = isOn7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            isOn8 = !isOn8;
+            button8.BackColor = isOn8 ? Color.Green : Color.Red;
+            button8.Text = isOn8 ? "ON" : "OFF";
+            buttonStates[7] = isOn8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            isOn9 = !isOn9;
+            button9.BackColor = isOn9 ? Color.Green : Color.Red;
+            button9.Text = isOn9 ? "ON" : "OFF";
+            buttonStates[8] = isOn9;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            isOn10 = !isOn10;
+            button10.BackColor = isOn10 ? Color.Green : Color.Red;
+            button10.Text = isOn10 ? "ON" : "OFF";
+            buttonStates[9] = isOn10;
+        }
+    }
+}
